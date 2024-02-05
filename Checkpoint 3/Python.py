1406d82069d42a737e87ede00ead7de975827aea
Aitor353 <149380411+Aitor353@users.noreply.github.com>
2024-02-05 15:12:04 +0100

    Add files via upload

---
+++ Checkpoint 3/Python.py
@@ -0,0 +1,31 @@
+#Execise 1 
+string_1 = "This is a string"
+number_1 = 512
+list_1 = ["item1", "item2", "item3"]
+boolean_1 = True
+
+#Execise 2 
+string_2 = string_1[:3]
+
+#Execise 3 
+list_2 = list_1[1]
+
+#Execise 4 
+number_2 = number_1 + 10
+
+#Execise 5
+list_3 = list_1[-1]
+
+#Execise 6
+names = "harry,alex,susie,jared,gail,conner"
+list_of_names = names.split(",")
+
+#Execise 7 
+string_4 = string_1[:4].upper()
+string_5 = string_4 + string_1[4:]
+
+#Execise 8 
+string_6 = f"This is a number {number_1}"
+
+#Execise 9 
+print("hello world")

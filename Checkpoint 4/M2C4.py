bf051651e0166ed6f369926be59772b67a708591
Aitor353 <149380411+Aitor353@users.noreply.github.com>
2024-02-13 11:38:06 +0100

    Add files via upload

---
+++ Checkpoint 4/M2C4.py
@@ -0,0 +1,39 @@
+from decimal import Decimal
+import math
+
+#Exercise 1
+
+my_list = ["f", "d", "b"]
+my_tuple = ("item1", "item2", "item3")
+my_float = 8.43
+my_integer = 50
+my_decimal = 1.0000000247
+my_dictionary = {
+    "1": "item1",
+    "2": "item2",
+    "3": "item3"
+}
+
+#Exercise 2
+my_float = math.ceil(my_float)
+
+#Exercise 3
+my_float = math.sqrt(my_float)
+
+#Exercise 4
+my_dictionary["1"]
+
+#Exercise 5
+my_tuple[1]
+
+#Exercise 6
+my_list.append("a")
+
+#Exercise 7
+my_list[0] = "z"
+
+#Exercise 8
+my_list.sort()
+
+#Exercise 9
+my_tuple += ("item4",)
